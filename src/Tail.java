bad8c8c364003d02a334913af59aa353ff7b4897
Rob Stewart <robstewart57@gmail.com>
2020-10-01 20:34:06 +0100

    Adds tail recursive tests

--- src/Tail.java
+++ src/Tail.java
@@ -2,20 +2,33 @@
 
 public class Tail {
 
-	public static int tailFactorial(int n,int res){
+	public static int factorial(int n) {
+		return -1; // TODO use tailFactorial
+	}
+	
+	private static int tailFactorial(int n,int res){
 		if (n == 0)
 			return res;
 		else 
 			return tailFactorial(n-1,n*res);
 	}
 	
+	public static int sum(int n) {
+		return -1; // TODO use tailSum
+	}
+	
 	// optional part
-	public static int tailSum(int n,int sum){
+	private static int tailSum(int n,int sum){
 		return -1; // dummy value to be removed.
 	}
 		
+	
+	public static int multiply(int m, int n) {
+		return -1; // TODO use tailMultiply
+	}
+	
 	// optional part
-	public static int tailMultiply(int m,int n,int sum){
+	private static int tailMultiply(int m,int n,int sum){
 		return -1; // dummy value to be removed.
 	}
 
